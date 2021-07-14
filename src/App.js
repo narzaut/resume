a91daeb41887ed8fd9a69b1afccb61c7d98033f3
narz <i.arzaut@itecriocuarto.org.ar>
2021-07-14 01:23:53 -0300

    remove routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import { HomePage } from './components/pages/HomePage'
-import { NotFound } from './components/pages/NotFound'
-import {  BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <Router>
-			<Switch>
-        <Route exact path="/" component={ HomePage } />
-				<Route component={ NotFound } />
-      </Switch>
-    </Router>
+    <HomePage />
   )
 }
 
